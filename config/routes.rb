f664bd5154002239625254e869f730d4a93c8ff5
kobayashiryou <chiri.parihead@gmail.com>
2021-07-13 11:27:22 +0900

    change routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  resources :todos
+  root "todos#index",format: "json"
+  resources :todos, only: [:create, :show, :update, :destroy]
+  post "callback", to: "line_bots#callback"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
