8445dd2f994eaea20c6cea3ed46e8b63bcefc9c7
kobayashiryou <chiri.parihead@gmail.com>
2021-07-16 06:37:20 +0900

    add function in line_bots_controller.rb

--- app/controllers/line_bots_controller.rb
+++ app/controllers/line_bots_controller.rb
@@ -16,6 +16,8 @@ class LineBotsController < ApplicationController
     events = client.parse_events_from(body)
 
     events.each do |event|
+      user_id = event["source"]["userId"]
+      user = User.find_by( uid: user_id) || User.create(uid: user_id)
       # LINE からテキストが送信された場合
       if (event.type === Line::Bot::Event::MessageType::Text)
         # LINE からテキストが送信されたときの処理を記述する
@@ -23,7 +25,8 @@ class LineBotsController < ApplicationController
         text =
           case message
           when "一覧"
-            todos = Todo.all
+            todos = user.todos
+
             #indexキーをつけて、.joinメソッドで長い文字列にする。
             #("\n")をつけると文字列を改行してくれる
             todos.map.with_index(1) { |todo, index| "#{index}: #{todo.title}" }.join("\n")
@@ -31,14 +34,14 @@ class LineBotsController < ApplicationController
             index = message.gsub(/完了*/, "").strip.to_i
             #受け取った文字が完了１（１０進数）ならば、そのmessageからgsubメソッドにより、完了の文字を削除して、"""で空白を入れる
             #空白をstripメソッドで空白を除き、残った数字を.to_iメソッドで文字列から数字にする。
-            todos = Todo.all.to_a
+            todos = user.todos.to_a
             #配列をArrayに変換してtodosに入れる
             todo = todos.find.with_index(1) {|_todo, _index| index == _index}
             #１からの_indexが付されたそれぞれのtodo(_todo)の_indexがindexと合致するものを探す
             todo.destroy!
             "Todo:#{index}が完了しました"
           else
-            Todo.create!(title: message)
+            user.todos.create!(title: message)
             "Todo:[#{message}]の登録が完了しました"
           end
 
